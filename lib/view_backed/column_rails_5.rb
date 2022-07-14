e71155f37d760fcbb1fb8afc492387e872cac466
Wilson Fong <wilsonmfong@gmail.com>
2022-07-14 09:45:52 -0400

    Add Rails 6.1 compatibility (#12)

--- lib/view_backed/column_rails_5.rb
+++ lib/view_backed/column_rails_5.rb
@@ -5,5 +5,16 @@ module ViewBacked
         name.to_s, default, self
       )
     end
+
+    # https://github.com/rails/rails/pull/35891
+    # Rails 6.1 added deduplication of AR schema cache structures to save memory
+    # when these structures are identical (i.e. same column names and types).
+    # The below code avoids deduplication, but in theory, we could add it by
+    # reimplementing ActiveRecord::ConnectionAdapters::Deduplicable.
+    if Rails.version.match(/^6.1/)
+      def deduplicate
+        self
+      end
+    end
   end
 end
